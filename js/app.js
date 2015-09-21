74b8213796650ff556b195abbbd118768733759d
hwahle <helenwahle@gmail.com>
2015-09-21 16:05:05 -0700

    fixed functionality so added items can move from to-do to done

--- js/app.js
+++ js/app.js
@@ -3,26 +3,29 @@ $(document).ready(function() {
 
 	
 	console.log('Hello');
+	
 
 
 	/*  submit button for the input field */
-		$('#add-button').click(function() {
-			if( $('input#add-items').val().trim().length == 0 ) {
-				/*alert("add item here")*/
-				$('#error').show();
-				$('input#add-items').val("");
-			} else {
-				$('#groceries').prepend('<li class="items">' + '<img class="complete">' + $('input#add-items').val() + '</li>');
-				$('input#add-items').val("");
-				$('#error').hide();
-			}
-		})
+	$('#add-button').click(function() {
+		if( $('input#add-items').val().trim().length == 0 ) {
+			/*alert("add item here")*/
+			$('#error').show();
+			$('input#add-items').val("");
+		} else {
+			var todoItem = $('<li class="items">' + '<img class="complete">' + $('input#add-items').val() + '</li>')
+			$('#groceries').prepend(todoItem);
+			$('input#add-items').val("");
+			$('#error').hide();
 
-	
 
-		/*use complete button to move completed items to done list below*/
-		$('.complete').on('click', function() {
-			$('#bought').prepend('<li class="done-items":first-child>' + '<img class="re-add">' + $( "li.items" ).text() + '<img class="delete">' + '</li>');
-			
+
+	/*use complete button to move completed items to done list below*/
+	todoItem.on('click', function() {
+		var reAdd = '<img class="re-add">'
+		var removeItem = '<img class="delete">'
+		$('#bought').prepend('<li class="done-items">' + reAdd + $( "li.items:first-child" ).text() + removeItem + '</li>');
+	})
+		}
 		})
 })
